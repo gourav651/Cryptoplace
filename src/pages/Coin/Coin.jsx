8d4087aeabb9b323bcaec36ad269f6d80fe3bee5
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2025-07-24 23:18:42 +0530

    Fix casing issue for LineChart component

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Coin.css";
 import { useParams } from "react-router-dom";
 import { CoinContext } from "../../context/CoinContext";
-import LineChart from "../../components/lineChart/lineChart";
+import LineChart from "../../components/LineChart/LineChart";
 const Coin = () => {
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState();
